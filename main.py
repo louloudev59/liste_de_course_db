7d85c0085322e521127abb6cd0df2f4ff56b7d31
louloudev59 <106763258+louloudev59@users.noreply.github.com>
2023-09-03 16:20:40 +0000

    Ajoute time.sleep(2)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import time
 
 liste = "liste.json"
 
@@ -22,28 +23,36 @@ while True:
         choix_user = input(menu)
         if choix_user not in choix:
             print("Veuillez choisir une option valide !")
+            time.sleep(3)
 
     if choix_user == "1":
         item = input("Entrez le nom d'un élément à ajouter à la liste de courses : ")
         liste.append(item)
         print(f"L'élément {item} a bien été ajouté à la liste.")
+        time.sleep(2)
     elif choix_user == "2":
         item = input("Entrez le nom d'un élément à retirer de la liste de courses : ")
         if item in liste:
             liste.remove(item)
             print(f"L'élément {item} a bien été supprimé de la liste.")
+            time.sleep(2)
         else:
             print(f"L'élément {item} n'est pas dans la liste.")
+            time.sleep(2)
     elif choix_user == "3":
         if liste:
             print("Voici le contenu de votre liste :")
             for i, item in enumerate(liste, 1):
                 print(f"{i}. {item}")
+                time.sleep(2)
+                
         else:
             print("Votre liste ne contient aucun élément.")
+            time.sleep(2)
     elif choix_user == "4":
         liste.clear()
         print("La liste a été vidée de son contenu.")
+        time.sleep(2)
     elif choix_user == "5":
         with open(liste, "w") as f:
             json.dump(liste, f, indent=4)
