27b0b8c09f35fbba30c24992dd159331a4bb5fcc
louloudev59 <106763258+louloudev59@users.noreply.github.com>
2023-09-03 16:16:59 +0000

    refonte main.py

--- main.py
+++ main.py
@@ -1,33 +1,53 @@
-import time
+import sys
 import json
 
-while True:
-    choix = int(input("Bienvenue dans votre liste de course. Merci de choisir parmi les 5 options suivantes laquelle voulez-vous exécuter :\n 1 : Afficher la liste de course\n 2 : Ajouter un élément dans la liste de course\n 3 : Supprimer un élément dans la liste de course\n 4 : Vider les éléments de la liste de courses\n 5 : Quitter le programme\n Votre choix : "))
+liste = "liste.json"
 
-    if choix < 1 or choix > 5:
-        print("Veuillez choisir un nombre valide !")
-        time.sleep(3)
-    else:
-        if choix == 1:
-            try:
-                with open("liste.json", "r") as f:
-                    donnees = json.load(f)
-                    if donnees:
-                        print(f"Actuellement, dans la liste de courses, vous avez : {donnees}")
-                    else:
-                        print("La liste de courses est vide.")
-                time.sleep(5)
-            except FileNotFoundError:
-                print("La liste de courses est vide.")
-                time.sleep(5)
-        elif choix == 2:
-            with open("liste.json", "a") as f:
-                ajouter = input("Quel élément souhaitez-vous ajouter : ")
-                if ajouter.strip():  # Vérifie si l'entrée n'est pas vide
-                    if not ajouter.endswith(", "):
-                        ajouter += ", "
-                    f.write(ajouter + "\n")
-                    print(f"L'élément {ajouter.strip(', ')} a bien été ajouté à la liste de course")
-                else:
-                    print("L'élément ne peut pas être vide.")
-                time.sleep(3)
+menu = """Choisissez parmi les 5 options suivantes :
+1: Ajouter un élément à la liste
+2: Retirer un élément de la liste
+3: Afficher la liste
+4: Vider la liste
+5: Quitter
+? Votre choix : """
+
+choix = ["1", "2", "3", "4", "5"]
+
+with open(liste, "r") as f:
+        liste = json.load(f)
+
+while True:
+    choix_user = ""
+    while choix_user not in choix:
+        choix_user = input(menu)
+        if choix_user not in choix:
+            print("Veuillez choisir une option valide !")
+
+    if choix_user == "1":
+        item = input("Entrez le nom d'un élément à ajouter à la liste de courses : ")
+        liste.append(item)
+        print(f"L'élément {item} a bien été ajouté à la liste.")
+    elif choix_user == "2":
+        item = input("Entrez le nom d'un élément à retirer de la liste de courses : ")
+        if item in liste:
+            liste.remove(item)
+            print(f"L'élément {item} a bien été supprimé de la liste.")
+        else:
+            print(f"L'élément {item} n'est pas dans la liste.")
+    elif choix_user == "3":
+        if liste:
+            print("Voici le contenu de votre liste :")
+            for i, item in enumerate(liste, 1):
+                print(f"{i}. {item}")
+        else:
+            print("Votre liste ne contient aucun élément.")
+    elif choix_user == "4":
+        liste.clear()
+        print("La liste a été vidée de son contenu.")
+    elif choix_user == "5":
+        with open(liste, "w") as f:
+            json.dump(liste, f, indent=4)
+        print("À bientôt !")
+        sys.exit()
+
+    print("-" * 50)
